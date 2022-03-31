aca8b2a89f32183deee1906ffae7586fdbb6f615
amir <abedi@sohagroup.org>
2022-03-31 17:51:51 +0430

    build project

---
+++ src/test/java/com/company/person/personManage/PersonManageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.company.person.personManage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonManageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
